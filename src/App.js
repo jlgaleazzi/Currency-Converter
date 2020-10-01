d3ce218399ddb25aadf65ecaa72578f3e40edef1
jlgaleazzi <jgaleazzi@gmail.com>
2020-09-30 22:59:10 -0500

    input working as expected

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const lcurr = [
+    { value: 1, label: "USD", symbol: "$" },
+    { value: 2, label: "EURO", symbol: "&euro" },
+  ];
+  const [baseSymbol, setBaseSymbol] = "$";
+  const [equivalentSymbol, setEquivalentSymbol] = "€";
+  const [baseCurrency, setBaseCurrency] = useState("");
+  const [equivalentCurrency, setEquivalentCurrency] = useState("");
+  const [currencies, setCurrencies] = useState(lcurr);
+  var tempString = "";
+
+  const handleBaseChange = (event) => {
+    let value = String(event.target.value).replace(".", "");
+
+    if (event.target.validity.valid) {
+      tempString += value;
+      setBaseCurrency(formatInput(tempString));
+    }
+  };
+
+  const focusBaseHandler = (event) => {
+    event.preventDefault();
+    let pos = event.target.value.length;
+    event.target.setSelectionRange(pos, pos);
+  };
+
+  const formatInput = (input) => {
+    return Number(input / 100).toFixed(2);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="input-currency">
+        <input
+          type="text"
+          value={baseCurrency}
+          onChange={handleBaseChange}
+          onFocus={focusBaseHandler}
+          //pattern="^-?[0-9]*$"
+          pattern="^-?[0-9]\d*\.?\d*$"
+        />
+        <i>{baseSymbol}</i>
+        <select>
+          {currencies.map((curr) => (
+            <option key={curr.value} value={curr.value}>
+              {curr.label}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <div className="input-currency">
+          <input type="text" value={equivalentCurrency} readOnly={true} />
+          <i>{equivalentSymbol}</i>
+          <select>
+            {currencies.map((curr) => (
+              <option key={curr.value} value={curr.value}>
+                {curr.label}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
     </div>
   );
 }
