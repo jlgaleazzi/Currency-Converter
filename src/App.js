e6729fd7a4fca7fab7ee748ad10fa642ba9e7af3
Juan Galeazzi <jgaleazzi@gmail.com>
2020-10-01 22:20:53 -0500

    working, still needs inline validation for symbols

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "./App.css";
+import fx from "money";
 
 function App() {
-  const lcurr = [
-    { value: 1, label: "USD", symbol: "$" },
-    { value: 2, label: "EURO", symbol: "&euro" },
-  ];
-  const [baseSymbol, setBaseSymbol] = "$";
-  const [equivalentSymbol, setEquivalentSymbol] = "€";
+
+  const [baseSymbol, setBaseSymbol] = useState("$");
+  const [equivalentSymbol, setEquivalentSymbol] = useState("€");
+  const [baseCode, setBaseCode] = useState("USD");
+  const [equivalentCode, setEquivalentCode] = useState("EUR");
   const [baseCurrency, setBaseCurrency] = useState("");
   const [equivalentCurrency, setEquivalentCurrency] = useState("");
-  const [currencies, setCurrencies] = useState(lcurr);
-  var tempString = "";
+  const [currencies, setCurrencies] = useState([]);
+  const [loadingCurrencies, setLoadingCurrencies] = useState(true);
+
+  const fetchRates = useCallback((code) => {
+    if (code === undefined) code = baseCode;
+    fx.base = code;
+    fetch(`https://api.exchangeratesapi.io/latest?base=${code}`)
+      .then(res => res.json())
+      .then(data => fx.rates = data.rates);
+
+  }, [baseCode])
+
+
+  useEffect(() => {
+    fetch("https://gist.githubusercontent.com/mddenton/062fa4caf150bdf845994fc7a3533f74/raw/27beff3509eff0d2690e593336179d4ccda530c2/Common-Currency.json")
+      .then(res => res.json())
+      .then(data => {
+        setCurrencies(Object.values(data));
+        setLoadingCurrencies(false);
+        fetchRates();
+      });
+  }, [fetchRates])
+
+
+  let tempString = "";
+
+
+  useEffect(() => {
+    // validate inputs 
+    if (baseCurrency !== "") {
+      let converted = fx(Number(baseCurrency)).from(baseCode).to(equivalentCode);
+      setEquivalentCurrency(converted.toFixed(2))
+    }
+
+  }, [baseCurrency, baseCode, equivalentCode])
+
+
+  const currencyChangeHandler = (evt) => {
+    let evtCode = evt.target.value;
+    let currency = currencies.find(({ code }) => code === evtCode);
+    if (evt.target.id === "baseDropDown") {
+      setBaseCode(evtCode);
+      setBaseSymbol(currency.symbol);
+    }
+    if (evt.target.id === "equivalentDropDown") {
+      setEquivalentCode(evtCode);
+      setEquivalentSymbol(currency.symbol);
+    }
+  }
 
   const handleBaseChange = (event) => {
     let value = String(event.target.value).replace(".", "");
-
     if (event.target.validity.valid) {
       tempString += value;
-      setBaseCurrency(formatInput(tempString));
+      let formattedValue = formatInput(tempString);
+      setBaseCurrency(formattedValue);
     }
   };
+
 
   const focusBaseHandler = (event) => {
     event.preventDefault();
@@ -40,14 +88,13 @@ function App() {
           value={baseCurrency}
           onChange={handleBaseChange}
           onFocus={focusBaseHandler}
-          //pattern="^-?[0-9]*$"
           pattern="^-?[0-9]\d*\.?\d*$"
         />
         <i>{baseSymbol}</i>
-        <select>
-          {currencies.map((curr) => (
-            <option key={curr.value} value={curr.value}>
-              {curr.label}
+        <select id="baseDropDown" onChange={currencyChangeHandler} disabled={loadingCurrencies} value={baseCode}>
+          {currencies.map((curr, i) => (
+            <option key={i} value={curr.code} >
+              {curr.code}
             </option>
           ))}
         </select>
@@ -56,10 +103,10 @@ function App() {
         <div className="input-currency">
           <input type="text" value={equivalentCurrency} readOnly={true} />
           <i>{equivalentSymbol}</i>
-          <select>
-            {currencies.map((curr) => (
-              <option key={curr.value} value={curr.value}>
-                {curr.label}
+          <select id="equivalentDropDown" onChange={currencyChangeHandler} disabled={loadingCurrencies} value={equivalentCode} >
+            {currencies.map((curr, i) => (
+              <option key={i} value={curr.code} >
+                {curr.code}
               </option>
             ))}
           </select>
